75a14bd6a58384d1470b5ab64b460ae3420f7b0d
CatherineChoi <hanchoice@gmail.com>
2017-10-18 14:37:30 -0400

    test

--- app/src/main/java/capstone/msd/conestoga/instantsalenotifier/MainActivity.java
+++ app/src/main/java/capstone/msd/conestoga/instantsalenotifier/MainActivity.java
@@ -22,12 +22,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+     //such change
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Catherine", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
