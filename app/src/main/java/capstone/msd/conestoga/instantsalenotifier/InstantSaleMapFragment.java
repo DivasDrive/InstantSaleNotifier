54e4bb6e141cafc81c022ff9baa5e759618e0a58
CatherineChoi <hanchoice@gmail.com>
2017-12-15 01:26:55 -0500

    UI Design Change - Login - Signup

---
+++ app/src/main/java/capstone/msd/conestoga/instantsalenotifier/InstantSaleMapFragment.java
@@ -0,0 +1,173 @@
+package capstone.msd.conestoga.instantsalenotifier;
+
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import capstone.msd.conestoga.instantsalenotifier.database.DataBaseConnection;
+import capstone.msd.conestoga.instantsalenotifier.location.PermissionUtils;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InstantSaleMapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
+    private String TAG = InstantSaleMapFragment.class.getSimpleName();
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient = null;
+    private LocationManager locationManager = null;
+    private boolean isPermissionGranted = false;
+    private DataBaseConnection connectionUtil = null;
+    private Connection conn =null;
+    public InstantSaleMapFragment() {
+        connectionUtil = new DataBaseConnection();
+        conn = connectionUtil.getConnection();
+        String selectSQL ="SELECT NAME FROM STORES ";
+        PreparedStatement pstmt =null;
+
+        try {
+            if (conn != null) {
+                pstmt = conn.prepareStatement(selectSQL);
+                ResultSet  rs =  pstmt.executeQuery();
+                while(rs.next()){
+                   Log.d(TAG, rs.getString(1));
+                }
+            }
+        }catch(SQLException e){
+            Log.e(TAG, e.getMessage());
+        }finally {
+            if(pstmt != null)
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    Log.e(TAG, e.getMessage() );
+                }
+             connectionUtil.close();
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sale_map, container, false);
+
+        PermissionUtils permissionUtils=new PermissionUtils(this.getContext());
+        ArrayList<String> permissions=new ArrayList<>();
+        permissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionUtils.check_permission(permissions,"Need GPS permission for getting your location",1);
+
+        locationManager = (LocationManager) this.getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this.getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //  return TODO;
+        }
+        if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        }else if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10.2f));*/
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        Geocoder geocoder = new Geocoder(getApplicationContext());
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latitude,longitude,1);
+            String str = addressList.get(0).getLocality()+",";
+            str+=  addressList.get(0).getCountryName();
+            mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,10.5f));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
